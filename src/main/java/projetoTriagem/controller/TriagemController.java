d32ca80f22786d5ebfd7b143a43660f3d68e9ce5
awdkzinha50 <oliveiraanna5010@gmail.com>
2026-02-27 19:16:55 -0300

    inicializacao de PessoaService dentro da classe Controller

--- src/main/java/projetoTriagem/controller/TriagemController.java
+++ src/main/java/projetoTriagem/controller/TriagemController.java
@@ -1,27 +1,26 @@
-package projetoTriagem;
+package projetoTriagem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import projetoTriagem.model.Pessoa;
+import projetoTriagem.service.PessoaService;
 
 
 @Controller
 public class TriagemController {
 
-    @GetMapping("/cadastro")
-    public String cadastro() {
-        return "cadastro";
-    }
+    @Autowired
+    private PessoaService pacienteService;
 
-    @PostMapping ("/salvarPaciente")
-    public String receberDadoHTML(Pessoa paciente) {
-        System.out.println("Nome do paciente: " + paciente.getNome());
-        System.out.println("Idade do paciente: " + paciente.getIdade());
-        System.out.println("CPF do paciente: " + paciente.getCPF());
+    @PostMapping("/salvarPaciente")
+    public String receberDadosHtml(Pessoa paciente) {
+        pacienteService.registroPaciente(paciente);
 
         return "redirect:/cadastro";
-        }
     }
+    
+    }
+
 
