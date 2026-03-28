df83817f32b89c08a2bd5c94061c968d9f9d2167
jennifer filgueira <jennifer.g.filgueira@gmail.com>
2026-03-28 14:38:53 -0300

    chore: correção de branch

---
+++ src/Pessoa.java
@@ -0,0 +1,38 @@
+public abstract class Pessoa {
+    
+    protected String nome;
+    protected String cpf;
+    protected int dataNascimento;
+
+    public Pessoa(String nome, String cpf, int dataNascimento){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(int dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+
+}
