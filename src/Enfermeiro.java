4dbe7f308840335597c2a369daf65983579a83ac
awdkzinha50 <oliveiraanna5010@gmail.com>
2026-03-28 17:43:21 -0300

    Atribuição da classe enfermeiro e médico

---
+++ src/Enfermeiro.java
@@ -0,0 +1,9 @@
+public class Enfermeiro extends ProfissionalSaude {
+    public Enfermeiro(String nome, String cpf, int dataNascimento, String registroConselho){
+        super(nome, cpf, dataNascimento, registroConselho);  
+    }
+
+    public void assinarFicha() {
+        
+    }
+}
---
+++ src/Medico.java
@@ -0,0 +1,22 @@
+public class Medico extends ProfissionalSaude {
+        public Medico(String nome, String cpf, int dataNascimento, String registroConselho){
+        super(nome, cpf, dataNascimento, registroConselho);  
+    }
+    public void encaminhamentoEmergencia() {
+
+    }
+
+    public void encaminhamentoEspecialista() {
+
+    }
+
+    public void encaminhamentoMedico() {
+
+    }  
+
+    public void avaliarFicha() {
+        
+    }
+
+
+}
