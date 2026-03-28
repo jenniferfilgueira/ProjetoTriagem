a5e9d48cd47cfeb42c58da57924da57ea97c4540
jennifer filgueira <jennifer.g.filgueira@gmail.com>
2026-03-28 16:07:23 -0300

    feature: implementação da class ProfissionalSaude

---
+++ src/ProfissionalSaude.java
@@ -0,0 +1,18 @@
+public abstract class ProfissionalSaude extends Pessoa{
+
+    protected String registroConselho;
+
+    public ProfissionalSaude(String nome, String cpf, int dataNascimento, String registroConselho){
+        super(nome, cpf, dataNascimento);
+        this.registroConselho = registroConselho;
+    }
+
+    public String getRegistroConselho() {
+        return registroConselho;
+    }
+
+    public void setRegistroConselho(String registroConselho) {
+        this.registroConselho = registroConselho;
+    }
+    
+}
