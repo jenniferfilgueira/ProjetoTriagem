1a5fd81c0b74672dc895df363e0d5a413936d0ae
jennifer filgueira <jennifer.g.filgueira@gmail.com>
2026-03-28 17:14:46 -0300

    feature: implementação da class paciente adulto

--- src/PacienteAdulto.java
+++ src/PacienteAdulto.java
@@ -1,3 +1,18 @@
-public class PacienteAdulto {
+public class PacienteAdulto extends Paciente {
     
+    public PacienteAdulto(String nome, String cpf, int dataNascimento, String registroConselho){
+        super(nome, cpf, dataNascimento, registroConselho);
+    }
+
+    @Override
+    public int calcularAgravanteRisco(){
+        if(2026 - this.dataNascimento >= 65){
+            return 2;
+        }else if(2026 - this.dataNascimento >= 50){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
 }
